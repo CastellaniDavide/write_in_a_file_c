61df8d13cefb8b6c14a3eeefda239bde526f059f
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-09-10 13:02:35 +0200

    Created bin/template.c

---
+++ bin/write_in_a_file_c.c
@@ -0,0 +1,22 @@
+/**
+ * @file write_in_a_file_c.cpp
+ *
+ * @version 01.01 2020-08-15
+ *
+ * @brief An example where I write in a file in c
+ *
+ * @ingroup write_in_a_file_c
+ * (Note: this needs exactly one @defgroup somewhere)
+ *
+ * @author Castellani Davide
+ *
+ * Contact: contacts@castellanidavide.it
+ *
+ */
+#include <stdio.h>
+
+int main() {
+	// printf() displays the string inside quotation
+	printf("write_in_a_file_c");
+	return 0;
+}
